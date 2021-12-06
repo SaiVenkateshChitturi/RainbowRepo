4ad8c1a8f444a9d38c3ee9e7b26bf34f0d95b25c
SaiVenkateshChitturi <sai_chitturi@outlook.com>
2021-12-06 17:40:55 +0530

    Add project files.

---
+++ coreVuewModelApplication/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreViewModelApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Student student = new Student()
+            {
+                StudentId = 1001,
+                Name = "King Kochhar",
+                Gender = "Male",
+                Subject = "Computer Science"
+            };
+            Address address = new Address()
+            {
+                StreetName = "Civil Lines",
+                City = "Delhi",
+                State = "Delhi",
+                Pincode = "110088",
+                Country = "India",
+                StudentId = 1001
+            };
+
+           return View(student);
+        }
+    }
+}
---
+++ coreVuewModelApplication/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreViewModelApplication
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Subject { get; set; }
+    }
+}
